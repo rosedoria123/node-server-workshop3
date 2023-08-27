ac0add82d3d80dc2e8e23b048e7137f38e999d6a
rosedoria123 <113267533+rosedoria123@users.noreply.github.com>
2023-08-27 07:14:39 -0500

    Add files via upload

---
+++ routes/partnerRouter.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const promotionRouter = express.Router();
+const authenticate = require('../authenticate');
+
+const Promotion = require('../models/promotion');
+
+promotionRouter.route('/')
+.get((req, res, next) => {
+    Promotion.find()
+    .then(promotions => res.status(200).json(promotions))
+    .catch(err => next(err))
+})
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Promotion.create(req.body)
+    .then(promotion => res.status(200).json(promotion))
+    .catch(err => next(err))
+})
+.put(authenticate.verifyUser, (req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /promotions');
+})
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Promotion.deleteMany()
+    .then(promotions => res.status(200).json(promotions))
+    .catch(err => next(err))
+});
+
+promotionRouter.route('/:promotionId')
+.get((req, res, next) => {
+    Promotion.findById(req.params.promotionId)
+    .then(promotion => res.status(200).json(promotion))
+    .catch(err => next(err))
+})
+.post(authenticate.verifyUser, (req, res) => {
+  res.statusCode = 403;
+  res.end(`POST operation not supported on /promotions/${req.params.promotionId}`);
+})
+.put(authenticate.verifyUser,authenticate.verifyAdmin, (req, res, next) => {
+    Promotion.findByIdAndUpdate(req.params.promotionId, req.body, { new: true })
+    .then(promotion => res.status(200).json(promotion))
+    .catch(err => next(err))
+})
+.delete(authenticate.verifyUser, authenticate.verifyAdmin,(req, res, next) => {
+    Promotion.findByIdAndDelete(req.params.promotionId)
+    .then(promotion => res.status(200).json(promotion))
+    .catch(err => next(err))
+});
+
+module.exports = promotionRouter;
